fda7c8187a14612cbb9083ec88284434d206d344
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-02-09 12:57:57 +0900

    chore: usage 수정

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main(image_path: str):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python main.py <image_path>")
+        print("Usage: python3 main.py <image_path>")
         sys.exit(1)
 
     image_path = sys.argv[1]
